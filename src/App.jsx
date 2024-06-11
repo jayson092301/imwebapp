56450fc4d758fe81c2fdee5ef04c05b9766ca6f5
jayson092301 <jaysondaniel805@gmail.com>
2024-06-12 00:22:42 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './page/dashboard';
+import LogIn from './page/logIn';
+
+function App() {
+  return (
+    <div>
+      <LogIn />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function dashboard() {
+    return(
+        <code>dashboard</code>
+    )
+}
+
+export default dashboard;
