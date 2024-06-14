0bdb6dc7d3bbef812a8e65979c47203f82dd4caa
jayson092301 <jaysondaniel805@gmail.com>
2024-06-14 17:08:50 +0800

    5:00 pm commit

--- src/dashboardcomponents/Prescription.jsx
+++ src/dashboardcomponents/Prescription.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Paper } from '@mui/material';
+import { Box, Paper, Button, TextField, Grid, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
 
 const Prescription = () => {
   return (
     <div>
       <Paper elevation={3}>
-      <div className="icon" style={{display:'flex'}}>
-        <div style={{margin:'8px'}}>
-          <img src="../../img/prescription.jpg" alt="Prescription" />
+        <div className="icon" style={{ display: 'flex' }}>
+          <div style={{ margin: '8px' }}>
+            <img src="../../img/prescription.jpg" alt="Prescription" />
+          </div>
+          <div>
+            <h2 style={{ marginLeft: '10px' }}>Prescription</h2>
+          </div>
         </div>
-        <div>
-          <h2 style={{marginLeft:'10px'}}>Prescription</h2>
-        </div>
-        
-      </div>
       </Paper>
+        <div style={{ margin:'15px'}}>
+          <div style={{textAlign:'center'}}>
+          <span><b>In-Patient</b></span>
+          </div>
+          <div>
+            <Grid item xs={6} sm={3}>
+              <FormControl fullWidth variant="outlined" size='small'>
+                <InputLabel>Patient Name</InputLabel>
+                <Select label="Patient Name" required>
+                  <MenuItem value="Name 1">Name 1</MenuItem>
+                  <MenuItem value="Name 2">Name 2</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </div>
+        </div>
       {/* Patient content goes here */}
     </div>
   );
